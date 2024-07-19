4697faba8bb32b4dc14e25a52bac6f28ff1a442a
Mattia Larentis <larentis@spaziodati.eu>
2024-07-19 14:02:24 +0200

    remove file

--- build.ts
+++
@@ -1,8 +0,0 @@
-import esbuild from 'esbuild';
-
-await esbuild.build({
-  entryPoints: [`admin/components/RenderJSON.tsx`],
-  platform: 'node',
-  loader: {'.node': 'file'},
-  outfile: `dist/RenderJSON.js`,
-});
